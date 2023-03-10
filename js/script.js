8ba6a1bfa1729c1f762f8404e8946d2fe454389e
LUCIANO KOHLER DA SILVA <luciano_k_silva@estudante.sesisenai.org.br>
2023-03-10 07:59:58 -0300

    progresso na aula

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var mapActivator = document.getElementById('mapactivate');
 
 var othersdiv = document.getElementById('others')
-mapActivator.addEventListener('click', function(){})
+mapActivator.addEventListener('click', function(){
+    othersdiv.style.display = "none";
+})
