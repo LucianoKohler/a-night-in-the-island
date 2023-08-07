38a696d808a65672e567abf3a8c74a71171923c5
LUCIANO KOHLER DA SILVA <luciano.silva2@alunos.sc.senac.br>
2023-08-07 15:11:13 -0300

    otimizada geral

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var Stick = false
+
+let del = 5
+//velocidade de escrita dos textos
 
 //BOTÕES PARA A TELA DE INÍCIO
 
@@ -15,45 +19,7 @@ document.addEventListener('keydown', function(event){
     }
 })
 
-//"BANCO DE DADOS" PARA AS FALAS DO JOGO
-//todos os <a> possuem a função de dar update no jogo, as variáveis inseridas são: a imagem a ser carregada e o texto a ser printado
 
-var chooseLines = {
-    0: `You can use the map anytime you want to leave the place`,
-}
-
-var startLines = {
-    0: `You wake up in front of a great and dense forest, without any memory of the last day, what happened?`,
-    1: `The only thing you can do now is to investigate, what will you do?`,
-    2: `<a href="#"> Enter the forest </a>`,
-    3: `<a href="#" onclick="updateScreen('map-choose', 'chooselocation')"> Retreat from the forest`,
-}
-
-var forestLines = {
-    0: `Back to forest you get, what will you do?`,
-    1: `<a href="#"> Enter the forest </a>`,
-    2: `<a href="#" onclick="updateScreen('map-choose', 'chooselocation')"> Retreat from the forest`,
-}
-
-var enterForestLines = {
-    0: `You decided to enter further into the forest`
-}
-
-
-var wellLines = {
-    0: ` In the distance, you discover an ordinary well with a hanging sign on its side. The sign reads: "Show me the shinies"` ,
-    1: ` What will you do?` ,
-    2: ` <a href="#"> Throw a Coin` ,
-    3: ` <a href="#" onclick="updateScreen('well_jump', 'well_jump')"> Jump into the well` ,
-}
-
-var well_jumpLines = {
-    0: `Without any fear, you hop into the well, after all, that's how games work!`,
-    1: `.`,
-    2: `.`,
-    3: `. right?`,
-    4: `After some time, you hit the ground and die instantly, what was you thinking!?`
-}
 
 
 
@@ -134,15 +100,16 @@ var chatDiv = document.getElementById('text');
 
 document.getElementById('startbutton').onclick = () =>{
     var twStart = new Typewriter(chatDiv, {
-        delay: 25,
+        delay: del,
       });
 
       twStart
       .pauseFor(500)
-      .typeString(startLines[0] + '<br> <br>')
+      .typeString(startLines[0])
       .pauseFor(1000)
-      .typeString(startLines[1] + '<br> <br>')
-      .typeString(startLines[2] + '<br>')
+      .typeString(startLines[1])
+      .typeString(WWYD)
+      .typeString(startLines[2])
       .typeString(startLines[3])
       .start()
 }
@@ -156,50 +123,74 @@ function updateScreen(nextImg, text){
     NI.classList.add('active')
 
     //mudar texto
+    let TW = new Typewriter(chatDiv, {
+        delay: del,
+    })
+
     switch (text){
+
         case 'chooselocation':
-            let TWS = new Typewriter(chatDiv, {
-                delay: 25,
-            });
-            TWS.typeString(chooseLines[0]).start()
+
+            TW.typeString(chooseLines[0]).start()
             break;
+
         case 'well':
-            let TWW = new Typewriter(chatDiv, {
-                delay: 25,
-            });
-            TWW.typeString(wellLines[0] + '<br> <br>')
+
+            TW.typeString(wellLines[0])
             .pauseFor(500)
-            .typeString(wellLines[1] + '<br> <br>')
-            .typeString(wellLines[2] + '<br>')
-            .typeString(wellLines[3])            
+            .typeString(WWYD)
+            .typeString(wellLines[1])
+            .typeString(wellLines[2])            
             .start()
             break;
+
         case 'well_jump':
-            let TWWJ = new Typewriter(chatDiv, {
-                delay: 25,
-            });
-            TWWJ.typeString(well_jumpLines[0] + "<br> <br>")
+
+            TW.typeString(well_jumpLines[0])
             .pauseFor(1000)
             .typeString(well_jumpLines[1])
             .pauseFor(1000)
             .typeString(well_jumpLines[2])
             .pauseFor(1000)
-            .typeString(well_jumpLines[3] + "<br> <br>")
+            .typeString(well_jumpLines[3])
             .pauseFor(2000)
             .typeString(well_jumpLines[4])
             .start()
-
-
             break;
+
         case 'forest':
-            let TWF = new Typewriter(chatDiv, {
-                delay: 25,
-            });
-            TWF.typeString(forestLines[0] + '<br> <br>')
+            TW.typeString(forestLines[0])
             .pauseFor(500)
-            .typeString(forestLines[1] + '<br>')
+            .typeString(WWYD)
+            .typeString(forestLines[1])
             .typeString(forestLines[2])            
             .start()
+            break;
+
+        case 'enterForest':
+
+
+            if(Stick){//com graveto
+                TW.typeString(enterForestLinesWithStick[0])
+                .typeString(WWYD)
+                .typeString(enterForestLinesWithStick[1])
+                .typeString(enterForestLinesWithStick[2])
+                .start()
+                
+            }else{//sem graveto
+                Stick = true
+                TW.typeString(enterForestLinesWOStick[0])
+                .pauseFor(500)
+                .typeString(enterForestLinesWOStick[1])
+                .pauseFor(1000)
+    
+                .typeString(WWYD)
+                .typeString(enterForestLinesWOStick[2])
+                .typeString(enterForestLinesWOStick[3])
+                .start()
+            }
+
+
             break;
 
         default:
@@ -210,3 +201,56 @@ function updateScreen(nextImg, text){
 
 UpdateColors()
 
+//"BANCO DE DADOS" PARA AS FALAS DO JOGO
+//todos os <a> possuem a função de dar update no jogo, as variáveis inseridas são: a imagem a ser carregada e o texto a ser printado
+//criei essa var abaixo pois é muito repetida pelo jogo
+var WWYD = 'What will you do? <br><br>'
+
+var chooseLines = {
+    0: `You can use the map anytime you want to leave the place`,
+}
+
+var startLines = {
+    0: `You wake up in front of a great and dense forest, without any memory of the last day, what happened? <br><br>`,
+    1: `The only thing you can do now is to investigate. <br><br>`,
+
+    2: `<a href="#" onclick="updateScreen('forest', 'enterForest')"> Enter the forest </a> <br>`,
+    3: `<a href="#" onclick="updateScreen('map-choose', 'chooselocation')"> Retreat from the forest`,
+}
+
+var forestLines = {
+    0: `Back to forest you get.<br><br>`,
+
+    1: `<a href="#" onclick="updateScreen('forest', 'enterForest')"> Enter the forest </a><br>`,
+    2: `<a href="#" onclick="updateScreen('map-choose', 'chooselocation')"> Retreat from the forest`,
+}
+
+var enterForestLinesWOStick = {
+    0: `You decided to enter further into the forest, unarmed and lost <br> <br>`,
+    1: `By the ground, you find a somewhat straight stick on the ground, you got <b>an ordinary stick</b>! <br><br>`,
+
+    2: `<a href="#">continue</a> <br>`,
+    3: `<a href="#" onclick="updateScreen('map-choose', 'chooselocation')"> Retreat from the forest`
+}
+
+var enterForestLinesWithStick = {
+    0: `Back to the same place you are, you can still see the spot where you found your stick... <br><br>`,
+    
+    1: `<a href="#">continue</a> <br>`,
+    2: `<a href="#" onclick="updateScreen('map-choose', 'chooselocation')"> Retreat from the forest`
+}
+
+var wellLines = {
+    0: ` In the distance, you discover an ordinary well with a hanging sign on its side. The sign reads: "Show me the shinies" <br><br>` ,
+
+    1: ` <a href="#"> Throw a Coin<br>` ,
+    2: ` <a href="#" onclick="updateScreen('well_jump', 'well_jump')"> Jump into the well` ,
+}
+
+var well_jumpLines = {
+    0: `Without any fear, you hop into the well, after all, that's how games work!<br><br>`,
+    1: `.`,
+    2: `.`,
+    3: `. right?<br><br>`,
+    4: `After some time, you hit the ground and die instantly, what was you thinking!?`
+}
